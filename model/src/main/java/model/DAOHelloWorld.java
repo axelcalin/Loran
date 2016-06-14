6a0f9e907678614791946fb38c894461127e2b6b
romain <romain@pc-romain>
2016-06-14 15:48:59 +0200

    14/06 part5

--- model/src/main/java/model/DAOHelloWorld.java
+++ model/src/main/java/model/DAOHelloWorld.java
@@ -1,18 +1,14 @@
 package model;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
-import javax.swing.text.Element;
+import view.HelloWorld;
 
-import org.Element.STONE;
+
 
 /**
  * The Class DAOHelloWorld.
@@ -20,10 +16,6 @@ import org.Element.STONE;
  * @author Jean-Aymeric Diet
  */
 class DAOHelloWorld extends DAOEntity<HelloWorld> {
-
-	private int width;
-	private int height;
-	private org.Element.Element elements[][];
 
 	/**
 	 * Instantiates a new DAO hello world.
@@ -100,12 +92,8 @@ class DAOHelloWorld extends DAOEntity<HelloWorld> {
 	 *
 	 * @see model.DAOEntity#find(java.lang.String)
 	 */
-	public HelloWorld find(final String keywords,final int width, final int height) {
-		this.width = width;
-		this.height = height;
-		this.elements = new org.Element.Element[this.getWidth()][this.getHeight()];
+	public HelloWorld find(final String keywords) {
 		HelloWorld helloWorld = new HelloWorld();
-
 		try {
 			final String sql = "{call mapByKey(?)}";
 			final CallableStatement call = this.getConnection().prepareCall(sql);
@@ -115,20 +103,6 @@ class DAOHelloWorld extends DAOEntity<HelloWorld> {
 			if (resultSet.first()) {
 				helloWorld = new HelloWorld(resultSet.getInt("id"), keywords, resultSet.getString("map"));
 			}
-			BufferedReader in;
-			String S = resultSet.getString("map");
-			ArrayList<String> map = new ArrayList<String>();
-			for(String temp : S.split("\n")) map.add(temp);
-
-for (int y = 0; y < map.size(); y++) {
-			for (int x = 0; x < map.get(y).length(); x++) {
-				switch(map.get(y).charAt(x)){
-				case 'P' :
-					addElement(new STONE(), x, y);
-					break;
-				}
-			}
-}
 			return helloWorld;
 		} catch (final SQLException e) {
 			e.printStackTrace();
@@ -136,33 +110,6 @@ for (int y = 0; y < map.size(); y++) {
 		return null;
 
 }
-private void addElement(org.Element.Element element, int x, int y) {
-		this.elements[x][y] = element;
-		
-	}
-
-public int getWidth() {
-	return this.width;
-}
-
-public int getHeight() {
-	return this.height;
-}
-
-public org.Element.Element getElements(final int x, final int y) {
-	if ((x < 0) || (y < 0) || (x >= this.getWidth()) || (y >= this.getHeight())) {
-		return null;
-	}
-	return this.elements[x][y];
-}
-
-@Override
-public HelloWorld find(String key) {
-	// TODO Auto-generated method stub
-	return null;
-}
-
-
 }
 
 	
