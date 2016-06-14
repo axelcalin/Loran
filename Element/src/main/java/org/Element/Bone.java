df7676881c9ce19f84c11c2548f8f50f4ab3cdde
Axel <Axel@PC-Axel>
2016-06-14 21:26:15 +0200

    part 1

--- Element/src/main/java/org/Element/Bone.java
+++
@@ -1,5 +0,0 @@
-package org.Element;
-
-public class Bone {
-
-}
--- Element/src/main/java/org/Element/Stone.java
+++
@@ -1,9 +0,0 @@
-package org.Element;
-
-
-public class Stone extends Element{
-
-		public Stone() {
-			super( Permeability.BLOCKING,new Sprite("â”Œâ”�", "bone.png"));
-		}
-}
