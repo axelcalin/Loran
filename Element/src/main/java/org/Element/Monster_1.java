c3aac4ebb8aa0bfffea9c5572e5151c79ba44b8e
romain <romain@pc-romain>
2016-06-16 14:26:07 +0200

    .

--- Element/src/main/java/org/Element/Monster_1.java
+++ Element/src/main/java/org/Element/Monster_1.java
@@ -2,10 +2,10 @@ package org.Element;
 
 import contract.Permeability;
 
-public class Monster_1 extends Mobile
+public class Monster_1 extends Monster
 {
 	public Monster_1()
 	{
-		super( new Sprite("â”Œâ”�", "monster_1.png"), /* TODO Create this monster's behavior */);
+		super( new Sprite("â”Œâ”�", "monster_1.png"), new Monster_1Behavior()/* TODO Create this monster's behavior */);
 	}
 }
