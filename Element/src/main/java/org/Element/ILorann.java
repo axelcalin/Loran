42b886237b21adacabd3abe264a4f56257948652
Axel <Axel@PC-Axel>
2016-06-16 14:25:25 +0200

    part

--- Element/src/main/java/org/Element/ILorann.java
+++ Element/src/main/java/org/Element/ILorann.java
@@ -2,9 +2,13 @@ package org.Element;
 
 public interface ILorann
 {
-		public void moveUp();
+	public void moveUp();
 	public void moveDown();
 	public void moveRight();
 	public void moveLeft();
+	public void moveUpRight();
+	public void moveUpLeft();
+	public void moveDownRight();
+	public void moveDownLeft();
 	
 }
