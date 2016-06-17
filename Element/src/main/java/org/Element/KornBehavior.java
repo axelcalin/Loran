af1549b1c53216cf16b3a3f7f6f753a7f938ae4b
Florent Desmet <Florent Desmet@10.144.128.23>
2016-06-17 15:56:53 +0200

    Implemented AIs

--- Element/src/main/java/org/Element/KornBehavior.java
+++ Element/src/main/java/org/Element/KornBehavior.java
@@ -1,10 +1,14 @@
 package org.Element;
+
+import java.util.Random;
+
 /**
  * The Class KornBehavior.
  *
  * @author florent, axel, luc, romain;
  */
 public class KornBehavior implements IMonsterBehavior {
+	private Monster master;
 	
 	/**
 	 * 
@@ -12,21 +16,38 @@ public class KornBehavior implements IMonsterBehavior {
 	public KornBehavior(){
 		
 	}
+	
+	/* (non-Javadoc)
+	 * @see org.Element.IMonsterBehavior#setMaster(org.Element.Monster)
+	 */
+	public void setMaster(Monster master){
+		this.master = master;
+	}
 
 	/* (non-Javadoc)
 	 * @see org.Element.IMonsterBehavior#animate()
 	 */
 	public void animate() {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see org.Element.IMonsterBehavior#setMaster(org.Element.Monster)
-	 */
-	public void setMaster(Monster master) {
-		// TODO Auto-generated method stub
-		
-	}
+		  Random r = new Random();
+		  int valeur = 1 + r.nextInt(4);
+		  switch(valeur){
+		   case 1:
+		   System.out.println("IL T'ATTAQUE !!!!!");
+		   this.master.moveUpRight();
+		   break;
+		   case 2:
+		   System.out.println("IL T'ATTAQUE !!!!!");
+		   this.master.moveUpLeft();
+		   break;
+		   case 3:
+		   System.out.println("IL T'ATTAQUE !!!!!");
+		   this.master.moveDownLeft();
+		   break;
+		   case 4:
+		   System.out.println("IL T'ATTAQUE !!!!!");
+		   this.master.moveDownRight();
+		   break;
+		  }
+		 }
 
 }
